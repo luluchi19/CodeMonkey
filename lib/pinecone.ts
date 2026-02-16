91106356663f13ad0c6a357b9d10986df8106008
Yang <realmonster2004@gmail.com>
2026-02-16 15:34:37 +0700

    config inngest, init pinecone and ai-sdk

---
+++ lib/pinecone.ts
@@ -0,0 +1,7 @@
+import {Pinecone} from "@pinecone-database/pinecone";
+
+export const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_DB_API_KEY!
+});
+
+export const pineconeIndex = pinecone.index({ name: 'codemonkey-vector-embeddings-v1' });
